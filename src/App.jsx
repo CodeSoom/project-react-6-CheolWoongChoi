8f5313a00eb860c899b854f315ee40edf067f19f
CheolWoong Choi <bluesky4381@naver.com>
2022-02-08 21:58:57 +0900

    Not Found 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '@/pages/HomePage';
 import SearchResultPage from '@/pages/SearchResultPage';
+import NotFoundPage from '@/pages/NotFoundPage';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/searchResult" element={<SearchResultPage />} />
+      <Route page="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
