e06ac44f7e2f9ec5bc0df2ddb5af26c3e459baea
CheolWoong Choi <bluesky4381@naver.com>
2022-02-11 00:34:56 +0900

    메인 색상 & 폰트 적용

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const primary = {
+  main: '#80deea',
+  light: '#b4ffff',
+  dark: '#4bacb8',
+};
+
+export const secondary = {
+  main: '#f48fb1',
+  light: '#ffc1e3',
+  dark: 'bf5f82',
+};
